37af3b85528ac7ee39b10b3d8ee69a91f3968bb7
shivendra3 <shivendrapratapsingh333@gmail.com>
2017-04-02 22:50:19 +0530

    List Comprehensions

---
+++ BasicDataTypes/3.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    lists=[[i,j,k] for i in xrange(x+1) for j in xrange(y+1) for k in xrange(z+1) if i+j+k!=n]
+    print lists
